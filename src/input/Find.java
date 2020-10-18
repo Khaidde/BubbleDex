b5e0847cfe1a523f32c20533203b4c986d5e216e
lukrita <67081095+lukrita@users.noreply.github.com>
2020-10-17 17:08:26 -0700

    not sure

--- src/input/Find.java
+++ src/input/Find.java
@@ -4,8 +4,9 @@ import java.util.Objects;
 
 // Take in a string name and return a person
 public class Find {
+  //not sure how the name is taken in
   public Person getPerson (String name) {
-    ArrayList<Person> search = Group.getPeople(); //I think the getPeople() call might be wrong(?)
+    ArrayList<Person> search = Group.getPeople(); //I think my getPeople() call might be wrong(?)
     for (int i = 0; i < search.size(); i++) {
       if (((search.get(i)).getName()).equals(name)) {
         return search.get(i);
