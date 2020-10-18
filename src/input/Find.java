8747e9dfbbfedbfe69162a230683ef85a2fe3532
lukrita <67081095+lukrita@users.noreply.github.com>
2020-10-17 19:33:00 -0700

    does it work?

--- src/input/Find.java
+++ src/input/Find.java
@@ -6,7 +6,7 @@ import java.util.Objects;
 public class Find {
   //not sure how the name is taken in
   public Person getPerson (String name) {
-    ArrayList<Person> search = Group.getPeople(); //I think my getPeople() call might be wrong(?)
+    ArrayList<Person> search = Windows.allPeople; //I think my getPeople() call might be wrong(?)
     for (int i = 0; i < search.size(); i++) {
       if (((search.get(i)).getName()).equals(name)) {
         return search.get(i);
